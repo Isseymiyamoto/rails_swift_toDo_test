8cd3ac65fd26034c9a37f4a1032d50b0b2db03c7
Isseymiyamoto <ants.can.walk@gmail.com>
2020-08-27 00:02:40 +0900

    inputContainerView01

--- toDoApp_rails/toDoApp_rails/Controllers/LoginController.swift
+++ toDoApp_rails/toDoApp_rails/Controllers/LoginController.swift
@@ -12,7 +12,14 @@ class LoginController: UIViewController{
     
     // MARK: - Properties
     
-    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("ログイン", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        return button
+    }()
     
     
     
@@ -21,12 +28,24 @@ class LoginController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemRed
+        configureUI()
     }
     
     
     
     // MARK: - Helpers
     
+    func configureContainerView(){
+        
+    }
+    
+    func configureUI(){
+        
+        configureGradientLayer()
+        
+        view.addSubview(loginButton)
+        loginButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 64, paddingLeft: 16, paddingRight: 16 ,height: 48)
+        loginButton.layer.cornerRadius = 6
+    }
     
 }
---
+++ toDoApp_rails/toDoApp_rails/View/InputContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  InputContainerView.swift
+//  toDoApp_rails
+//
+//  Created by 宮本一成 on 2020/08/27.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class InputContainerView: UIView {
+    
+    init(image: UIImage?, textField: UITextField) {
+        super.init(frame: .zero)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
