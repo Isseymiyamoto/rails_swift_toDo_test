1b1bcea5adb117314404d79e5a5f2012900884d7
Isseymiyamoto <ants.can.walk@gmail.com>
2020-08-26 23:22:08 +0900

    xCodeファイル、フォルダ作成

---
+++ toDoApp_rails/toDoApp_rails/Utils/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  toDoApp_rails
+//
+//  Created by 宮本一成 on 2020/08/26.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import Foundation
---
+++ toDoApp_rails/toDoApp_rails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDoApp_rails
+//
+//  Created by 宮本一成 on 2020/08/26.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
