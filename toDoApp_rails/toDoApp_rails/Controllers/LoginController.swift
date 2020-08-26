5f936fcefd6f1ba65cb01323be341483af37972f
Isseymiyamoto <ants.can.walk@gmail.com>
2020-08-26 23:48:28 +0900

    authファイルの設定

---
+++ toDoApp_rails/toDoApp_rails/Controllers/LoginController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginController.swift
+//  toDoApp_rails
+//
+//  Created by 宮本一成 on 2020/08/26.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController{
+    
+    // MARK: - Properties
+    
+    
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed
+    }
+    
+    
+    
+    // MARK: - Helpers
+    
+    
+}
---
+++ toDoApp_rails/toDoApp_rails/Controllers/RegistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationController.swift
+//  toDoApp_rails
+//
+//  Created by 宮本一成 on 2020/08/26.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController{
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    
+    
+    
+    // MARK: - Helpers
+    
+    
+    
+}
