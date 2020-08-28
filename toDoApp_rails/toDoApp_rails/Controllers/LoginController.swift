4f64a7b7a3eb81af93bc315227c6865f0112a9e2
Isseymiyamoto <ants.can.walk@gmail.com>
2020-08-28 10:51:01 +0900

    login controller finish

--- toDoApp_rails/toDoApp_rails/Controllers/LoginController.swift
+++ toDoApp_rails/toDoApp_rails/Controllers/LoginController.swift
@@ -12,6 +12,13 @@ class LoginController: UIViewController{
     
     // MARK: - Properties
     
+    private let iconImage: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "pencil.and.outline")
+        iv.tintColor = .white
+        return iv
+    }()
+    
     private let emailTextField = CustomTextField(placeholder: "Email")
     
     private let passwordTextField = CustomTextField(placeholder: "Password")
@@ -28,13 +35,23 @@ class LoginController: UIViewController{
     
     private let loginButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("ログイン", for: .normal)
+        button.setTitle("Log In", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
         button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setHeight(height: 48)
+        button.layer.cornerRadius = 6
         return button
     }()
     
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedText = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [.foregroundColor: UIColor.white, .font: UIFont.systemFont(ofSize: 16)])
+        attributedText.append(NSAttributedString(string: "Sign Up", attributes: [.font: UIFont.boldSystemFont(ofSize: 16), .foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributedText, for: .normal)
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
     
     
     // MARK: - Lifecycle
@@ -45,6 +62,19 @@ class LoginController: UIViewController{
         configureUI()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowSignUp(){
+        let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
     
     
     // MARK: - Helpers
@@ -57,17 +87,29 @@ class LoginController: UIViewController{
         
         configureGradientLayer()
         
-        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 120, width: 120)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
         stack.axis = .vertical
-        stack.alignment = .leading
-        stack.spacing = 32
+        stack.spacing = 16
         
         view.addSubview(stack)
-        stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 64, paddingLeft: 16, paddingRight: 16)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor,
+                     paddingTop: 32, paddingLeft: 32, paddingRight: 32)
         
-        view.addSubview(loginButton)
-        loginButton.anchor(top: stack.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 64, paddingLeft: 16, paddingRight: 16 ,height: 48)
-        loginButton.layer.cornerRadius = 6
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor,
+                                     bottom: view.safeAreaLayoutGuide.bottomAnchor,
+                                     right: view.rightAnchor,
+                                     paddingLeft: 32,
+                                     paddingRight: 32)
+        
+        
     }
     
 }
--- toDoApp_rails/toDoApp_rails/Controllers/RegistrationController.swift
+++ toDoApp_rails/toDoApp_rails/Controllers/RegistrationController.swift
@@ -12,12 +12,61 @@ class RegistrationController: UIViewController{
     
     // MARK: - Properties
     
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleRegistration), for: .touchUpInside)
+        button.setHeight(height: 50)
+        return button
+    }()
+    
+    private let alreadyHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedText = NSMutableAttributedString(string: "Already have an account?  ", attributes: [.foregroundColor: UIColor.white, .font: UIFont.systemFont(ofSize: 16)])
+        attributedText.append(NSAttributedString(string: "Log In", attributes: [.font: UIFont.boldSystemFont(ofSize: 16), .foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributedText, for: .normal)
+        button.addTarget(self, action: #selector(handleShowLogin), for: .touchUpInside)
+        return button
+    }()
+    
     
     // MARK: - Lifecycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleShowLogin(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func handleRegistration(){
+        
+    }
     
     // MARK: - Helpers
+    
+    func configureUI(){
+        
+        configureGradientLayer()
+        
+    }
     
     
     
